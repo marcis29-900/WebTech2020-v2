9eada7d8b1e92719d458911010e99eaa2155b4b8
marcis29-900 <marcisparups@inbox.lv>
2020-06-08 23:06:58 +0300

    v2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,7 +15,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    logged in
                 </div>
             </div>
         </div>
